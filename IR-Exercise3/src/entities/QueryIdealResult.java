88334271ac4d5436b9614b58977a5844e1d45638
avihad <avihad87@gmail.com>
2014-05-03 16:12:28 +0300

    add DCG and NDCG support

--- IR-Exercise3/src/entities/QueryIdealResult.java
+++ IR-Exercise3/src/entities/QueryIdealResult.java
@@ -1,68 +1,74 @@
 package entities;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import utils.Pair;
-
 public class QueryIdealResult {
 
     private final int queryId;
-    private final List<Pair<Integer, Integer>> docsIdToRank;
+    private final Map<Integer, Integer> docsIdToRank;
     private final Map<Integer, Integer> docsByRank;
-    
+
     public QueryIdealResult(int queryId) {
 
 	this.queryId = queryId;
-	this.docsIdToRank = new ArrayList<Pair<Integer, Integer>>();
+	this.docsIdToRank = new HashMap<Integer, Integer>();
 	this.docsByRank = new HashMap<Integer, Integer>();
     }
-    
-    public int getSize()
-    {
-    	return this.docsIdToRank.size();
+
+    public void addResult(int docId, int rank) {
+
+	this.docsIdToRank.put(docId, rank);
+	this.docsByRank.put(rank, docId);
+
     }
 
-    public boolean addResult(int docId, int rank) {
+    private double calcDCG(List<Integer> docRank) {
 
-	boolean result = this.docsIdToRank.add(Pair.of(docId, rank));
-	
-	if(result)
-	{
-		docsByRank.put(rank, docId);
+	if (docRank.isEmpty()) {
+	    return 0;
 	}
-	
+
+	double DCG = docRank.get(0);
+	for (int i = 1; i < docRank.size(); i++) {
+	    DCG += docRank.get(i) / (Math.log(i + 1) / Math.log(2));
+	}
+
+	return DCG;
+    }
+
+    public double calcNDCG(List<Integer> docList) {
+	List<Integer> docRank = new ArrayList<Integer>();
+	for (Integer docId : docList) {
+	    docRank.add(this.docsIdToRank.getOrDefault(docId, 0));
+	}
+	double DCG = calcDCG(docRank);
+	Collections.sort(docRank);
+	double IDCG = calcDCG(docRank);
+
+	return IDCG != 0 ? DCG / IDCG : 0;
+    }
+
+    public int getDocByRank(int rank) {
+	Integer result = this.docsByRank.get(rank);
+
+	if (result == null) {
+	    result = -1;
+	}
+
 	return result;
     }
-    
-    public int getDocByRank(int rank)
-    {
-    	Integer result = docsByRank.get(rank);
-    	
-    	if(result == null)
-    	{
-    		result = -1;
-    	}
-    	
-    	return result;
+
+    public int getDocRank(int docId) {
+
+	return this.docsIdToRank.get(docId);
     }
-    
-    public int getDocRank(int docId)
-    {
-    	int result = -1;
-    	
-    	for(Pair<Integer, Integer> pair : docsIdToRank)
-    	{
-    		if(pair.first == docId)
-    		{
-    			result = pair.second;
-    			break;
-    		}
-    	}
-    	
-    	return result;
+
+    public int getSize() {
+	return this.docsIdToRank.size();
     }
 
 }
